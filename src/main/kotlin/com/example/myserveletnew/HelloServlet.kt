a80ea5e543db0c736e7191320cbcd11956bfff22
santosh123456789000 <140676178+santosh123456789000@users.noreply.github.com>
2025-11-04 10:47:39 +0900

    New Date Update HelloServlet.kt

--- src/main/kotlin/com/example/myserveletnew/HelloServlet.kt
+++ src/main/kotlin/com/example/myserveletnew/HelloServlet.kt
@@ -14,7 +14,7 @@ class HelloServlet : HttpServlet() {
     public override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
         response.contentType = "text/html"
 
-        // Hello
+        // Hello 2025/11/04
         val out = response.writer
         out.println("<html><body>")
         out.println("<h1>$message</h1>")
